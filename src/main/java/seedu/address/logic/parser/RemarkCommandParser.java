36bfdcdbf3882f67b13456d38c4b2cccfe6dacf1
shurvirarora <shurvirarora@gmail.com>
2022-02-24 20:40:18 +0800

    Update RemarkCommand, RemarkCommandParser class to use Remark class instead of plain String

--- src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -8,6 +8,7 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.RemarkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Remark;
 
 public class RemarkCommandParser {
 
@@ -31,6 +32,6 @@ public class RemarkCommandParser {
 
         String remark = argMultimap.getValue(PREFIX_REMARK).orElse("");
 
-        return new RemarkCommand(index, remark);
+        return new RemarkCommand(index, new Remark(remark));
     }
 }
